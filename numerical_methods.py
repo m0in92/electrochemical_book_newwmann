f005454612c90386d69c27ca0bdc3784fb179efe
m0in92 <moinahmed100@gmail.com>
2024-01-24 13:36:08 -0500

    Added functionality for the determinant calculation of tridiagonal matrix

--- numerical_methods.py
+++ numerical_methods.py
@@ -18,3 +18,36 @@ def first_derivative_cd_dirichlet_bc(n: int) -> npt.ArrayLike:
     ones[0] = 0
     return np.diag(zeros) + np.diag(neg_ones, -1) + np.diag(ones, 1)
 
+
+def tridiagonal_matrix_det(a: np.ndarray, b: np.ndarray, c: np.ndarray) -> float:
+    """
+    This function solves for the determinant of a tridiagonal matrix.
+    :param a: The vector containing the entries of the major diagonal
+    :param b: The vector containing the entries of the upper diagonal.
+    :param c: The vector containing the entries of the lower diagonal.
+    :return: (float) determinant of the tridiagonal matrix
+    """
+    if len(b) != len(a) - 1 | len(c) != len(a) - 1:
+        raise ValueError("Length of vector b or c should be an order less than that of vector a")
+    if len(a) < 3:
+        raise ValueError("The length of the diagonal matrix should be at-least 3.")
+    n: int = len(a)
+    f_n: list = []
+    for i in range(n):
+        if i == 0:
+            f_n.append(a[0])
+        elif i == 1:
+            f_n.append(a[i]*f_n[i-1]-c[i-1]*b[i-1])
+        else:
+            f_n.append(a[i]*f_n[i-1]-c[i-1]*b[i-1]*f_n[i-2])
+    return f_n[-1]
+
+
+def F(n: int, a: np.ndarray, b: np.ndarray, c: np.ndarray):
+    if n == 0 or n == -1:
+        return n+1
+    else:
+        print(a[n] * F(n-1, a[n-1], b[n-2], c[n-2]) - c[n-2] * b[n-2] * F(n-2, a[n-2], b[n-2], c[n-2]))
+        return a[n] * F(n-1, a[n-1], b[n-2], c[n-2]) - c[n-2] * b[n-2] * F(n-2, a[n-2], b[n-2], c[n-2])
+
+
--- tests/test_numerical_methods.py
+++ tests/test_numerical_methods.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from numerical_methods import first_derivative_cd, first_derivative_cd_dirichlet_bc
+from numerical_methods import first_derivative_cd, first_derivative_cd_dirichlet_bc, tridiagonal_matrix_det
 
 
 class TestFirstDerivative(unittest.TestCase):
@@ -21,3 +21,12 @@ class TestFirstDerivative(unittest.TestCase):
                                              [0, 0, 0, -1, 0, 1],
                                              [0, 0, 0, 0, 0, 1]])
         self.assertTrue(np.array_equal(final_result, first_derivative_cd_dirichlet_bc(n)))
+
+    class TestTridiagonalMatrix(unittest.TestCase):
+        def test_determinant(self):
+            a: np.ndarray = np.array([-2, -2, -2, -2, -2])
+            b: np.ndarray = np.array([1, 1, 1, 1])
+            c: np.ndarray = np.array([1, 1, 1, 1])
+            det: float = tridiagonal_matrix_det(a=a, b=b, c=c)
+
+            self.assertEqual(-6, det)
